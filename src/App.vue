1622d916397d400a2624cd25361fa54d0409afa5
PING NGE <ping.nge@hotmail.com>
2020-09-21 22:26:57 +1000

    UPDATE: basic todo functions complete, working on creating new todo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ title }}</h1>
+    <TodoList />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from "@/components/TodoList ";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TodoList
+  },
+  data() {
+    return {
+      title: "ToDo App",
+    };
+  },
+};
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss"></style>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="add-todo">
+    <form @submit.prevent="addTodo">
+      <input type="text" name="task" v-model="todo.task" />
+    </form>
+  </div>
+</template>
+
+<script>
+import moment from "moment";
+import shortid from "shortid";
+
+export default {
+  name: "AddTodo",
+  data() {
+    return {
+      todo: {
+        id: null,
+        task: "",
+        date: null,
+      },
+    };
+  },
+  methods: {
+    addTodo() {
+      return {
+        id: this.todo.id = shortid.generate(),
+        task: this.todo.task,
+        date: this .todo.date = moment().format("ll")
+      }
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
